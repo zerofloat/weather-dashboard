6cdb9f2bfeb43d4c2d351554339891a96a102e54
zerofloat <133889261+zerofloat@users.noreply.github.com>
2024-01-29 16:57:13 +0000

    replace JS syntax with jQuery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 console.log("JS loaded");
-
-var city = "";
-var queryURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=e7868cd101d6d925934597a0f7faa75e
-`;
-var 
+var city = $("search-input").val();
+// .val().trim();
+console.log(city);
+var key = "e7868cd101d6d925934597a0f7faa75e";
+var currentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
+var fiveDayForecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${key}`;
