0263d52f6085d90ef158b3b0720fdd2860beefea
zerofloat <133889261+zerofloat@users.noreply.github.com>
2024-02-01 15:36:17 +0000

    add forecast object, array and loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,15 +14,19 @@ var cityCurrentHum;
 var currentTempEl;
 var currentWindEl;
 var currentHumEl;
+var forecastEach;
+var forecastObj = {};
+var forecastArray = [];
 
 console.log('JS loaded');
 
 // https://stackoverflow.com/questions/1026069/how-do-i-make-the-first-letter-of-a-string-uppercase-in-javascript
-function capitalizeFirstLetter (string) {
-    return string.charAt(0).toUpperCase() + string.slice(1)};
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+};
 
-
-$('#search-button').on('click' , function () {
+//get user input and API vars
+$('#search-button').on('click', function () {
     event.preventDefault();
     cityInput = $('#search-input').val().trim();
     city = capitalizeFirstLetter(cityInput);
@@ -35,32 +39,65 @@ $('#search-button').on('click' , function () {
             return response.json();
         })
         .then(function (data) {
-            
+            //validate city input then concatenate fetched data into strings to store
             if (data.cod !== '404') {
-            $('#today').empty();
-            todayDate = dayjs().format('DD/MM/YYYY');
-            cityCurrentTemp = 'Temp: ' + (data.main.temp - 273.15).toFixed(2) + ' \u00B0C';
-            cityCurrentWind = 'Wind: ' + data.wind.speed.toFixed(1) + ' KPH';
-            cityCurrentHum = 'Humidity: ' + data.main.humidity + '%';
-            cityCurrentIcon = data.weather[0].icon;
-            console.log(cityCurrentIcon);
-            iconURL = `https://openweathermap.org/img/wn/${cityCurrentIcon}@2x.png`;
-            
-            
-            todayDiv = $('#today');
-            iconImage = $('<img>').attr('src', iconURL);
-            todayHeading = $('<h2>').text(`${city} (${todayDate})`).append(iconImage);
-            currentTempEl = $('<p>').text(`${cityCurrentTemp}`);
-            currentWindEl = $('<p>').text(`${cityCurrentWind}`);
-            currentHumEl = $('<p>').text(`${cityCurrentHum}`);
+                $('#today').empty();
+                todayDate = dayjs().format('DD/MM/YYYY');
+                cityCurrentTemp = 'Temp: ' + (data.main.temp - 273.15).toFixed(2) + ' \u00B0C';
+                cityCurrentWind = 'Wind: ' + data.wind.speed.toFixed(1) + ' KPH';
+                cityCurrentHum = 'Humidity: ' + data.main.humidity + '%';
+                cityCurrentIcon = data.weather[0].icon;
+                console.log(cityCurrentIcon);
+                iconURL = `https://openweathermap.org/img/wn/${cityCurrentIcon}@2x.png`;
 
-            todayDiv.append(todayHeading, currentTempEl, currentWindEl, currentHumEl);
+                //create elements for stored strings
+                todayDiv = $('#today');
+                iconImage = $('<img>').attr('src', iconURL);
+                todayHeading = $('<h2>').text(`${city} (${todayDate})`).append(iconImage);
+                currentTempEl = $('<p>').text(`${cityCurrentTemp}`);
+                currentWindEl = $('<p>').text(`${cityCurrentWind}`);
+                currentHumEl = $('<p>').text(`${cityCurrentHum}`);
 
-        } else {
-            todayDiv = $('#today');
-            todayHeading = $('<h2>').text(`${city} is not a city, please try again!`);
-            todayDiv.append(todayHeading);
-        }
+                //render to page
+                todayDiv.append(todayHeading, currentTempEl, currentWindEl, currentHumEl);
+                //failed input validation
+            } else {
+                $('#today').empty();
+                todayDiv = $('#today');
+                todayHeading = $('<h2>').text(`${city} is not a city, please try again!`);
+                todayDiv.append(todayHeading);
+            }
 
         })
+    fetch(fiveDayForecastURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            forecastArray = [];
+            //iterate through loop to store forecast for next 5 days
+            for (let i = 0; i < 5; i++) {
+                forecastEach = data.list[i];
+                forecastObj = {
+
+                    temperature: forecastEach.main.temp,
+                    wind_speed: forecastEach.wind.speed,
+                    humidity: forecastEach.main.humidity
+                
+                };
+            
+                forecastArray = JSON.parse(localStorage.getItem('forecastArray')) || [];
+                forecastArray.push(forecastObj);
+                localStorage.setItem('forecastArray', JSON.stringify(forecastArray));
+                // console.log(forecastEach.main.temp,
+                // forecastEach.wind.speed,
+                // forecastEach.main.humidity);
+
+                // forecastArray.push(forecastEach.main.temp, forecastEach.wind.speed, forecastEach.main.humidity);
+            }
+            console.log(forecastArray);
+        }
+            // forecastArray.push(forecastEach.main.temp, forecastEach.wind.speed, forecastEach.main.humidity);
+        )
+
 })
